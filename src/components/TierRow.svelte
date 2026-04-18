1430cea4e510daff1ddad526d32af82b42eb9a7d
galexand <giordana.alex@gmail.com>
2026-04-18 16:52:40 +0200

    feat: add TierRow component

---
+++ src/components/TierRow.svelte
@@ -0,0 +1,43 @@
+<script>
+  import TierLabel from './TierLabel.svelte';
+  import ItemGrid from './ItemGrid.svelte';
+  import { tierActions } from '../stores.js';
+
+  export let tier;
+
+  function handleRename(newLabel) {
+    tierActions.rename(tier.id, newLabel);
+  }
+
+  function handleColorChange(newColor) {
+    tierActions.setColor(tier.id, newColor);
+  }
+
+  function handleDelete() {
+    tierActions.remove(tier.id);
+  }
+
+  function handleItemsChange(newItems) {
+    tierActions.setItems(tier.id, newItems);
+  }
+</script>
+
+<div class="row">
+  <TierLabel
+    label={tier.label}
+    color={tier.color}
+    onRename={handleRename}
+    onColorChange={handleColorChange}
+    onDelete={handleDelete}
+  />
+  <ItemGrid items={tier.items} onChange={handleItemsChange} />
+</div>
+
+<style>
+  .row {
+    display: flex;
+    align-items: stretch;
+    border-bottom: 1px solid #2a2a2a;
+    min-height: 96px;
+  }
+</style>
